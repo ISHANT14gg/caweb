7c1abc441e2d0c97278ff8201f3554e72a14444d
jexo <sharmaishant9418556349@gmail.com>
2026-02-22 17:32:27 +0530

    chore(security): implement strict CSP, XFO, and XCTO headers

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,40 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://images.unsplash.com;
+    font-src 'self' data:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    connect-src 'self' https://*.supabase.co wss://*.supabase.co ws://localhost:* http://localhost:*;
+`;
+
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
